2d01152de26f84650a460db8d69312b96cef9ff3
Drakensis <sebastian.quijanob@gmail.com>
2014-08-14 12:49:07 -0500

    Create Area.java
    Clase Area que agrupa las areas en objetos que contienen area y codigo de area.

---
+++ Area.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Brownie
+ */
+public class Area {
+    private String area;
+    private String codigo;
+    
+    public Area(String codigo, String area){
+        this.area = area;
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the area
+     */
+    public String getArea() {
+        return area;
+    }
+
+    /**
+     * @param area the area to set
+     */
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
